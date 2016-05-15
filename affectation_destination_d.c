463a171fba9d668f895a24053790751e04ea976c
Sandra MICHAUT <sandra.michaut@ensiie.fr>
2016-05-15 17:46:13 +0200

    Modification pour supprimer des erreurs de compilation

--- affectation_destination_d.c
+++ affectation_destination_d.c
@@ -6,8 +6,11 @@
 #include <stdlib.h>
 #include "affectation_destination_d.h"
 #include "structure_d.h"
+#include "structure_c.h"
 
-void affectationCliLibre(destination ** croisPlan, destination ** croisSat, destination ** croisVie, destination ** croisDispo) {
+contraintes ** importer_contraintes();
+
+void affectationCliLibre(croisiere croisPlan, croisiere croisSat,croisiere croisVie, croisiere croisDispo) {
 
   /* On a besoin en entree:
      Tableaux des contraintes => A approvisionner dans le pgm
@@ -17,7 +20,7 @@ void affectationCliLibre(destination ** croisPlan, destination ** croisSat, dest
      ......
   */
   int n = 150; /* Nb touristes */
-  int i;
+  int i,j;
 
   /* Calcul des nouveaux quotas */
   
@@ -41,59 +44,59 @@ void affectationCliLibre(destination ** croisPlan, destination ** croisSat, dest
   
   
   /* Creation du tableau de structures destination et remplissage */
-  destination ** croisTot = creer_destination();
+  croisiere croisTot = creer_croisiere();
   setCrois(croisTot, croisDispo, croisPlan, croisSat, croisVie); // A voir
   
   
+  char ** destins = malloc(sizeof(char*));
   
   /* 1e parcours du tableau : verification des contraintes bidirectionnelles */
   for (i=0; i<n; i++) { // Boucle sur le tableau de dataCliLibre
-    char ** destins = malloc(sizeof(char*));
-
+    
     /* Recuperation des destinations dans un tableau */
-    destins[0] = getDes1(dataTotal[i]);
-    destins[1] = getDes2(dataTotal[i]);
-    destins[2] = getDes3(dataTotal[i]);
-    destins[3] = getDes4(dataTotal[i]);
-    destins[4] = getDes5(dataTotal[i]);
-    destins[5] = getDes6(dataTotal[i]);
+    destins[0] = getDes1(&dataTotal[i]);
+    destins[1] = getDes2(&dataTotal[i]);
+    destins[2] = getDes3(&dataTotal[i]);
+    destins[3] = getDes4(&dataTotal[i]);
+    destins[4] = getDes5(&dataTotal[i]);
+    destins[5] = getDes6(&dataTotal[i]);
     
     
-    for (j=0; j<nbContBi; j++) {
-      if (destins[ContBi[0].zone] == ContBi[0].nom && destins[ContBi[1].zone] != ContBi[1].nom) {
+    for (j=0; j<nbContBi; j++) { // Boucle sur les contraintes
+      if (destins[contBi[j][0].zone] == contBi[j][0].nom && destins[contBi[j][1].zone] != contBi[j][1].nom) {
 	/* La contrainte n'est pas respectee */
-	destins[ContBi[0].zone] = NULL;
+	destins[contBi[j][0].zone] = NULL;
       }
-      else if (destins[ContBi[1].zone] == ContBi[1].nom && destins[ContBi[0].zone] != ContBi[0].nom) {
+      else if (destins[contBi[j][1].zone] == contBi[j][1].nom && destins[contBi[j][0].zone] != contBi[j][0].nom) {
 	/* La contrainte n'est pas respectee */
-	destins[ContBi[1].zone] = NULL;
+	destins[contBi[j][1].zone] = NULL;
       }
     }
     
     for (j=0; j<6; j++) {
       if (destins[j] != NULL) {
-	// Decrementer la structure destination
+	// Decrementer la structure destination pour chaque destination
       }
-    
+      
+    }
   }
-
-
+  
   /* 2e parcours du tableau : verification des contraintes unidirectionnelles */
- for (i=0; i<n; i++) { // Boucle sur le tableau de dataCliLibre
-
+  for (i=0; i<n; i++) { // Boucle sur le tableau de dataCliLibre
+    
     /* Recuperation des destinations dans un tableau */
-    destins[0] = getDes1(dataTotal[i]);
-    destins[1] = getDes2(dataTotal[i]);
-    destins[2] = getDes3(dataTotal[i]);
-    destins[3] = getDes4(dataTotal[i]);
-    destins[4] = getDes5(dataTotal[i]);
-    destins[5] = getDes6(dataTotal[i]);
+    destins[0] = getDes1(&dataTotal[i]);
+    destins[1] = getDes2(&dataTotal[i]);
+    destins[2] = getDes3(&dataTotal[i]);
+    destins[3] = getDes4(&dataTotal[i]);
+    destins[4] = getDes5(&dataTotal[i]);
+    destins[5] = getDes6(&dataTotal[i]);
     
     
     for (j=0; j<nbContUni; j++) {
-      if (destins[ContUni[1].zone] == ContUni[1].nom && destins[ContUni[0].zone] != ContUni[0].nom) {
+      if (destins[contUni[j][1].zone] == contUni[j][1].nom && destins[contUni[j][0].zone] != contUni[j][0].nom) {
 	/* La contrainte n'est pas respectee */
-	destins[ContUni[0].zone] = ContUni[0];
+	destins[contUni[j][0].zone] = contUni[j][0].nom;
       }
     }
     
@@ -101,51 +104,36 @@ void affectationCliLibre(destination ** croisPlan, destination ** croisSat, dest
       if (destins[j] != NULL) {
 	// Decrementer la structure destination
       }
-    
+      
+    }
   }
-
-
+  
   /* 3e parcours du tableau (ordre decroissant) : annulation des places en trop */
- for (i=n-1; i>=0; i--) { // Boucle sur le tableau de dataCliLibre
+  /* On regarde le nombre de places au maximum qu'il faut enlever et on fait une boucle a partir de la fin jusqu'a arriver a ce nombre */
 
-    /* Recuperation des destinations dans un tableau */
-    destins[0] = getDes1(dataTotal[i]);
-    destins[1] = getDes2(dataTotal[i]);
-    destins[2] = getDes3(dataTotal[i]);
-    destins[3] = getDes4(dataTotal[i]);
-    destins[4] = getDes5(dataTotal[i]);
-    destins[5] = getDes6(dataTotal[i]);
+  for (i=n-1; i>=0; i--) { // Boucle sur le tableau de dataCliLibre
     
-    
-    for (j=0; j<nbContUni; j++) {
-      if (destins[ContUni[1].zone] == ContUni[1].nom && destins[ContUni[0].zone] != ContUni[0].nom) {
-	/* La contrainte n'est pas respectee */
-	destins[ContUni[0].zone] = ContUni[0];
-      }
-    }
-    
-    for (j=0; j<6; j++) {
-      if (destins[j] != NULL) {
-	// Decrementer la structure destination
-      }
     
   }
-
-
+  
   /* 4e parcours du tableau : remplissage des cases vides par destinations */
-
-
-
+  
+  
+  
   
   /* Exportation sous le format .csv */
-
-
-
+  
+  
+  
   /* On libere les structures utilisees */
+  free(destins);
   free(dataTotal);
   printf("Done\n");
  
 }
+
+
+
 
 int main(int argc, char ** argv) {
   
@@ -171,7 +159,6 @@ int main(int argc, char ** argv) {
   setNom(&dataTotal[0], nom2);
   printf("Le nom est %s\n", getNom(&dataTotal[0]));
   
-  affectationCliLibre();
   
   free(dataTotal);
   
